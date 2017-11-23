dde8dd6d9a72ad7ff4dce06470015fa4fb282a90
kidvan777 <ivankidyaev.ik@gmail.com>
2017-11-23 17:17:19 +0300

    first

---
+++ app/src/main/java/com/devslopes/flaborfit/DetailsActivity.java
@@ -0,0 +1,36 @@
+package com.devslopes.flaborfit;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        TextView exerciseText = (TextView)findViewById(R.id.exerciseTitle);
+        ImageView exerciceImage = (ImageView)findViewById(R.id.exerciseImage);
+        LinearLayout mainBG = (LinearLayout)findViewById(R.id.mainBG);
+
+        String exerciseTitle = getIntent().getStringExtra(MainActivity.EXTRA_ITEM_TITLE);
+        exerciseText.setText(exerciseTitle);
+
+        if(exerciseTitle.equalsIgnoreCase(MainActivity.EXERCISE_WEIGHTS)){
+            exerciceImage.setImageDrawable(getResources().getDrawable(R.drawable.weight,
+                getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#2ca5f5"));
+        }else if(exerciseTitle.equalsIgnoreCase(MainActivity.EXERCISE_YOGA)){
+            exerciceImage.setImageDrawable(getResources().getDrawable(R.drawable.lotus,                        getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#916bcd"));
+        }else{
+            exerciceImage.setImageDrawable(getResources().getDrawable(R.drawable.heart,                        getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#52ad56"));
+        }
+    }
+}
